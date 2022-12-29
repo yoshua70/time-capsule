c22ea7466c08455ca1a7904097054332cc8a9eb2
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 10:18:49 +0100

    feat: add login with google button

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,7 +1,7 @@
 export const LoginForm = () => {
   return (
-    <form>
-      <h1>Login form</h1>
-    </form>
+    <div className="flex flex-col items-center justify-center bg-white drop-shadow-lg rounded-lg px-12 py-8">
+      <button className="bg-gray-300 px-4 py-2 font-bold hover:bg-slate-200 transition-all duration-100 ease-in-out">Login with Google</button>
+    </div>
   )
 }
