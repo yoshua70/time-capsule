a4df729f6d5afbdf7b9d5136caddfa83c4fd7160
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 12:32:58 +0100

    feat: add zod resolver

--- src/components/forms/NewCapsuleForm.tsx
+++ src/components/forms/NewCapsuleForm.tsx
@@ -1,8 +1,14 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
 
 type Inputs = {
   content: string;
 };
+
+const schema = z.object({
+  content: z.string(),
+});
 
 export const NewCapsuleForm = () => {
   const {
@@ -10,7 +16,7 @@ export const NewCapsuleForm = () => {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({ resolver: zodResolver(schema) });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
@@ -20,7 +26,7 @@ export const NewCapsuleForm = () => {
       className="flex flex-col items-center justify-center"
     >
       <h1>New capsule</h1>
-      <input {...register("content")} />
+      <input {...register("content")} type="text" />
       <button
         type="submit"
         className="bg-gray-300 px-4 py-2 font-bold transition-all duration-100 ease-in-out hover:bg-slate-200"
