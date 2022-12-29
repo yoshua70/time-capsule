626540e73f19c66999576b0cd6dd4100369c172e
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 12:29:59 +0100

    feat: add content input field
    Add react hook form to the new capsule form.
    Add input field for the capsule's content.

--- src/components/forms/NewCapsuleForm.tsx
+++ src/components/forms/NewCapsuleForm.tsx
@@ -1,7 +1,32 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  content: string;
+};
+
 export const NewCapsuleForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-center justify-center"
+    >
       <h1>New capsule</h1>
+      <input {...register("content")} />
+      <button
+        type="submit"
+        className="bg-gray-300 px-4 py-2 font-bold transition-all duration-100 ease-in-out hover:bg-slate-200"
+      >
+        Create
+      </button>
     </form>
   );
 };
