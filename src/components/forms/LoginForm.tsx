ad1f8de063f951b6cee8671e686de43d11357f0a
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 10:21:34 +0100

    feat: adapt button text on user auth state

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,7 +1,13 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+
 export const LoginForm = () => {
+  const {data: sessionData} = useSession();
+
   return (
     <div className="flex flex-col items-center justify-center bg-white drop-shadow-lg rounded-lg px-12 py-8">
-      <button className="bg-gray-300 px-4 py-2 font-bold hover:bg-slate-200 transition-all duration-100 ease-in-out">Login with Google</button>
+      <button className="bg-gray-300 px-4 py-2 font-bold hover:bg-slate-200 transition-all duration-100 ease-in-out">
+        {sessionData ? "Sign out" : "Login with Google"}
+      </button>
     </div>
   )
 }
