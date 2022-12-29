cc6b1e4d0b14e4b8de0b2db63fff953c62d3b07f
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 13:08:36 +0100

    feat: add availability input

--- src/components/forms/NewCapsuleForm.tsx
+++ src/components/forms/NewCapsuleForm.tsx
@@ -1,19 +1,23 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import z from "zod";
+import Script from "next/script";
 
 interface IFormInputs {
   content: string;
+  availableIn: string;
 }
 
 const schema = z.object({
   content: z.string(),
+  availableIn: z.string(),
 });
 
 export const NewCapsuleForm = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<IFormInputs>({ resolver: zodResolver(schema) });
 
@@ -22,17 +26,28 @@ export const NewCapsuleForm = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col items-center justify-center"
+      className="flex flex-col items-center justify-center gap-8"
     >
       <h1>New capsule</h1>
-      <input {...register("content")} type="text" />
-      <p className="text-red-500">{errors.content?.message}</p>
+      <div className="flex flex-1 flex-col">
+        <textarea {...register("content")} className="bg-gray-300 px-4 py-4" />
+        <p className="text-red-500">{errors.content?.message}</p>
+      </div>
+      <div className="flex w-full flex-1 flex-col">
+        <select {...register("availableIn")} className="bg-gray-300 px-4 py-4">
+          <option>1 hour</option>
+        </select>
+        <p className="text-red-500">{errors.availableIn?.message}</p>
+      </div>
       <button
         type="submit"
         className="bg-gray-300 px-4 py-2 font-bold transition-all duration-100 ease-in-out hover:bg-slate-200"
       >
         Create
       </button>
+      <>
+        <Script src="https://unpkg.com/flowbite@1.5.5/dist/datepicker.js" />
+      </>
     </form>
   );
 };
