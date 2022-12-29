8ede229b7fa742ad70085fec69a7c3efc86e3a13
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 12:23:47 +0100

    feat: create new capsule page

---
+++ src/pages/capsule/new.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const NewCapsulePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center rounded-lg bg-white px-12 py-8 drop-shadow-lg">
+          <h1>Create a new capsule</h1>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default NewCapsulePage;
