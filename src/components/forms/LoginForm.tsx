84a422ae8733fc54b5b143280fb463907bb1fc4b
Michée Allidjinou <allidjinoumichee@outlook.fr>
2022-12-29 10:22:28 +0100

    feat: add on clik function to login button

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -5,7 +5,7 @@ export const LoginForm = () => {
 
   return (
     <div className="flex flex-col items-center justify-center bg-white drop-shadow-lg rounded-lg px-12 py-8">
-      <button className="bg-gray-300 px-4 py-2 font-bold hover:bg-slate-200 transition-all duration-100 ease-in-out">
+      <button className="bg-gray-300 px-4 py-2 font-bold hover:bg-slate-200 transition-all duration-100 ease-in-out" onClick={sessionData ? () => signOut() : () => signIn()}>
         {sessionData ? "Sign out" : "Login with Google"}
       </button>
     </div>
